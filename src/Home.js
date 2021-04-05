bff70aead10e38851c67951dc799f19d1b485033
Muhammed Osama <MuhammedOsama.Dev@gmail.com>
2021-04-05 16:41:52 +0200

    Create Structure

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Components
+import Navbar from './Layout/components/Navbar';
+
+export default function Home() {
+    return(
+        <div className="App">
+            <Navbar />
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Global Style
+import 'bootstrap-v4-rtl/dist/css/bootstrap-rtl.min.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import 'normalize.css';
+import './Layout/Main.scss'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
