9ad631e25845929fd44a881708842bc0f1d161c8
Muhammed Osama <MuhammedOsama.Dev@gmail.com>
2021-04-05 19:00:30 +0200

    Create Order Component

--- src/Home.js
+++ src/Home.js
@@ -3,12 +3,14 @@ import React from 'react';
 // Components
 import Navbar from './Layout/components/Navbar';
 import Routes from './Layout/components/Routes';
+import AddOrder from './Layout/components/AddOrder';
 
 export default function Home() {
     return(
         <div className="App">
             <Navbar />
             <Routes />
+            <AddOrder />
         </div>
     );
 }
