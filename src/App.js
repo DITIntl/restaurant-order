693d9aa674f2b4d687e3f1007e62a5b23d9cc7ee
Muhammed Osama <MuhammedOsama.Dev@gmail.com>
2021-04-05 16:51:15 +0200

    Create Navbar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {BrowserRouter,Switch, Route} from 'react-router-dom';
 
 // Components
 import Home from './Home';
+import Navbar from './Layout/components/Navbar';
 
 export default function App() {
       return (
             <div className="App">
                 <BrowserRouter>
+                    <Navbar />
                     <Switch>
                         <Route path="/" exact component={Home} />
                     </Switch>
--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 // Components
-import Navbar from './Layout/components/Navbar';
 
 export default function Home() {
     return(
         <div className="App">
-            <Navbar />
         </div>
     );
 }
